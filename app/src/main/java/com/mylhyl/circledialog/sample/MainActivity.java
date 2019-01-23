8a33eac24b665701405a685075b5429b42057122
石头 <48025211@qq.com>
2019-01-23 12:04:08 +0800

    纯文本模式支持上下滑动

--- app/src/main/java/com/mylhyl/circledialog/sample/MainActivity.java
+++ app/src/main/java/com/mylhyl/circledialog/sample/MainActivity.java
@@ -124,17 +124,55 @@ public class MainActivity extends AppCompatActivity implements BaseQuickAdapter.
                 break;
             case 1:
                 new CircleDialog.Builder()
+                        .setMaxHeight(0.8f)
                         .setCanceledOnTouchOutside(false)
                         .setCancelable(false)
                         .configDialog(params -> {
 //                            params.backgroundColor = Color.DKGRAY;
 //                            params.backgroundColorPress = Color.BLUE;
                         })
-                        .setTitle("标题")
-                        .setSubTitle("副标题")
-                        .setText("冷却风扇口无异物，风机扇叶无损伤，无过热痕迹")
+                        .setTitle("移动认证简介")
+                        .setSubTitle("更新日期:2019-01-16")
+                        .setText("一、移动认证简介\n" +
+                                "\n" +
+                                "1、什么是移动认证\n" +
+                                "\n" +
+                                "移动认证，基于运营商独有的网关能力+大数据能力，以手机号码作为开放的统一账号体系，为各类应用提供全面的用户账号使用和用户数据管理的一站式解决方案，实现身份认证、鉴权、管理的新型认证技术。目前已推出一键登录、本机号码校验两大产品，已为爱奇艺、支付宝、小米、同花顺等多款热门APP提供服务。\n" +
+                                "\n" +
+                                "1.jpg\n" +
+                                "\n" +
+                                "2、移动认证的特点\n" +
+                                "\n" +
+                                "● 以手机号码作为账号，通过确认手机号码，实现用户身份唯一性的认证。\n" +
+                                "\n" +
+                                "● 可使用户在应用注册登录环节，耗时仅1.5秒。\n" +
+                                "\n" +
+                                "● 适用于在移动手机客户端注册/登录/号码校验等多种场景。\n" +
+                                "\n" +
+                                "● 基于运营商网络认证结果，可避免短验或密码被拦截、攻击。\n" +
+                                "\n" +
+                                "3、移动认证能实现哪些能力\n" +
+                                "\n" +
+                                "目前已推出一键登录和本机号码校验两大产品。\n" +
+                                "二、移动认证能做什么\n" +
+                                "\n" +
+                                "1、一键登录\n" +
+                                "\n" +
+                                "一键登录能力，即通过移动认证的网络认证能力，实现APP用户无需输入帐号密码，即可使用本机手机号码自动登录的能力。利用应用层无法截取的网络层号码认证能力验证号码的真实性，本机号码自动校验是现有短信验证方式的优化，能消除现有短信验证模式等待时间长、操作繁琐和容易泄露的痛点。\n" +
+                                "\n" +
+                                "一键登录的能力优势\n" +
+                                "\n" +
+                                "● 降低应用注册/登录门槛，减轻用户记忆负担，提高用户体验；\n" +
+                                "\n" +
+                                "● 降低对用户身份、通信行为等属性验证的繁琐步骤，助力企业完善风险管控系统\n" +
+                                "\n" +
+                                "● 取号成功率高达99.8%。\n" +
+                                "\n" +
+                                "● 两步完成注册登录，耗时仅需1.5秒。\n" +
+                                "\n" +
+                                "● 节省企业短验成本")
                         .configText(params -> {
-//                                params.gravity = Gravity.LEFT | Gravity.TOP;
+                                params.gravity = Gravity.LEFT | Gravity.TOP;
 //                            params.padding = new int[]{100, 0, 100, 50};
                         })
                         .setNegative("取消", null)
